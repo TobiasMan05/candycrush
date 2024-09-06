ce070f63f2c7175a842d26731ee2da8e1e1d407f
milagrosfucci <110426141+milagrosfucci@users.noreply.github.com>
2024-09-06 14:06:45 -0300

    Update candycrush.asm

--- candycrush.asm
+++ candycrush.asm
@@ -1,44 +1,43 @@
-.ORIG x3000      ; Start of the program
+.ORIG x3000     
 
-        LD R0, RED_COLOR     ; Load red color into R0
-        LD R1, START_ADDR    ; Load start address (top-left corner) into R1
+        LD R0, rojo   
+        LD R1, start   
 
-        LD R2, WIDTH         ; Load the width of the square (16 pixels) into R2
-        LD R3, HEIGHT        ; Load the height of the square (15 pixels) into R3
-        LD R6, NUM_SQUARES   ; Load the number of squares (8) into R6
+        LD R2, ancho         
+        LD R3, altura       
+        LD R6, cant_cuadrados
 
 DrawSquares
-        ; Draw one row (16 pixels wide)
+        
 DrawRow
-        LD R4, WIDTH         ; Reset pixel counter (R4 = 16)
+        LD R4, ancho         
 DrawPixel
-        STR R0, R1, #0       ; Store the red color at memory address in R1
-        ADD R1, R1, #1       ; Move to the next pixel on the same row
-        ADD R4, R4, #-1      ; Decrement the pixel counter
-        BRp DrawPixel        ; If there are more pixels to draw in the row, repeat
 
-        ; Move to the next row
-        LD R4, SCREEN_WIDTH  ; Load the screen width (128)
-        ADD R4, R4, #-16     ; Subtract the width of the square (128 - 16)
-        ADD R1, R1, R4       ; Move to the beginning of the next row
+        STR R0, R1, #0       
+        ADD R1, R1, #1       
+        ADD R4, R4, #-1   
+           
+        BRp DrawPixel        
+        
+        LD R4, SCREEN_WIDTH  
+        ADD R4, R4, #-16     
+        ADD R1, R1, R4       
 
-        ADD R4, R4, #-1      ; Decrement the row counter
-        BRp DrawRow          ; If more rows remain, repeat
+        ADD R4, R4, #-1      
+        BRp DrawRow          
+        
+        ADD R1, R1, #1       
+        LD R3, altura         
+        ADD R6, R6, #-1      
+        BRp DrawSquares      
 
-        ; Move to the next square (1 pixel gap)
-        ADD R1, R1, #1       ; Move to the start of the next square
-        LD R3, HEIGHT        ; Reset row counter (R3 = 15)
-        ADD R6, R6, #-1      ; Decrement the square counter
-        BRp DrawSquares      ; If more squares remain, repeat
+        HALT                 
 
-        HALT                 ; End the program
 
-; Data section
-RED_COLOR      .FILL xF800    ; RGB value for red color (5-6-5 format)
-START_ADDR     .FILL xC000    ; Starting address of the display (top-left corner)
-WIDTH          .FILL #16      ; Width of the square (16 pixels)
-HEIGHT         .FILL #15      ; Height of the square (15 pixels)
-SCREEN_WIDTH   .FILL #128     ; Width of the screen (128 pixels)
-NUM_SQUARES    .FILL #8       ; Number of squares to draw (8 squares)
-
+rojo    .FILL xF800   
+start     .FILL xC000    
+ancho         .FILL #16      
+altura       .FILL #15      
+SCREEN_WIDTH   .FILL #128     
+cant_cuadrados    .FILL #8       
         .END
